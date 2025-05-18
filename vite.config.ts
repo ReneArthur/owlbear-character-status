d8c0af1322fab19d01fc9a6442a40030c7de387f
Rene Arthur <rene_arthur@outlook.com>
2025-05-17 21:00:27 -0300

    fixed cors policy

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,10 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    cors: {
+      origin: "https://www.owlbear.rodeo",
+    },
+  },
   plugins: [react(), tsconfigPaths()],
 });
